fe22fd3d7dba29da1bbd3e7fb849adc3e1ce7fe2
muhamad760 <muhamadgardy760@gmail.com>
2019-11-03 23:54:38 +0300

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/authors');
+
+//all authors route
+router.get('/',async (req,res)=>{
+    let searchOption = {};
+        if(req.query.name!==null&&req.query.name!==''){
+            searchOption.name = new RegExp(req.query.name,'i')
+        }
+    try {
+        const authors = await Author.find(searchOption)
+        res.render('authors/index',
+        { authors: authors,
+        searchOption: req.query
+        });
+    } catch{
+        res.redirect('/');
+    }
+     
+});
+
+//new authors route
+router.get('/new',(req,res)=>{
+    res.render('authors/new',{author : new Author()})
+});
+
+router.post('/',async (req,res)=>{
+    const author = new Author({
+        name : req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+         //res.redirect(`authors/${newAuthor.id}`);
+         res.redirect(`authors`);
+     } catch (err) {
+         res.render('authors/new',{
+             author : author,
+             errorMessage : 'Error cannot create author'
+         });  
+     }
+});
+
+module.exports = router;
